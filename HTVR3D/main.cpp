38d0c6a8b9c5ae2aca88032adfe199b3bdfc99c2
Ryo EGAMI <ryo_ega@Ryo-no-MacBook-Air.local>
2013-09-04 15:47:17 +0900

    Initial Commit

---
+++ HTVR3D/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HTVR3D
+//
+//  Created by Ryo EGAMI on 2013/09/04.
+//  Copyright (c) 2013年 Ryo EGAMI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
